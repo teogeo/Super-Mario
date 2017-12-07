98ae6b2e300c6199cb56d0f9cdfe4d243b389c86
Georgiadis Theodoros <32556102+teogeo@users.noreply.github.com>
2017-12-07 16:48:27 +0200

    Create Menu.js

---
+++ Menu.js
@@ -0,0 +1,27 @@
+var Menu = {
+preload: function() {
+    game.load.image('Menu', 'assets/menu.png');
+		game.load.image('Level1', 'assets/level1.png');
+		game.load.audio('house', 'audio/house.mp3');
+		},
+    
+    create: function() {
+		
+			house = game.add.audio('house');
+			house.play();
+
+			game.scale.pageAlignHorizontally = true;
+			game.scale.pageAlignVertically = true;
+			game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+			game.physics.startSystem(Phaser.Physics.ARCADE);
+			
+			var menuPic = game.add.sprite(8, 8, 'Menu');
+
+			var StageText = game.add.text(38, 150, 'Level1', {fontSize: '12px', fill: '#000'});
+			var click1 = game.add.button(60, 197, 'Level1', function() {
+			game.state.start('Level1');
+			house.stop();
+		});
+		click1.anchor.set(0.5, 0.5);
+	}
+}
